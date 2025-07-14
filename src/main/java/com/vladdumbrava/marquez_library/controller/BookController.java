68cd691fcf2b389eec4af1eab4e084e9db9c68bc
vlad.dumbrava <vladdumbrava5@gmail.com>
2025-07-14 12:44:15 +0300

    CRUD operations for books

--- src/main/java/com/vladdumbrava/marquez_library/controller/BookController.java
+++ src/main/java/com/vladdumbrava/marquez_library/controller/BookController.java
@@ -5,6 +5,8 @@ import com.vladdumbrava.marquez_library.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
@@ -12,9 +14,30 @@ public class BookController {
 
     private final BookService bookService;
 
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
     @PostMapping
     public Book createBook(@RequestBody Book book) {
         return bookService.createBook(book);
     }
 
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+    }
+
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable("id") Long id, @RequestBody Book newBook) {
+        return bookService.updateBook(id, newBook);
+    }
+
+    @PatchMapping("/{id}")
+    public Book updateBookTotalCopiesNumber(@PathVariable("id") Long id,
+                                            @RequestBody Integer newTotalCopiesNumber) {
+        return bookService.updateBookTotalCopiesNumber(id, newTotalCopiesNumber);
+    }
 }
--- src/main/java/com/vladdumbrava/marquez_library/service/BookService.java
+++ src/main/java/com/vladdumbrava/marquez_library/service/BookService.java
@@ -2,9 +2,12 @@ package com.vladdumbrava.marquez_library.service;
 
 import com.vladdumbrava.marquez_library.model.Book;
 import com.vladdumbrava.marquez_library.repository.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +20,41 @@ public class BookService {
         log.info("Created book with title: {}",book.getBookTitle());
         return bookRepository.save(book);
     }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    public Book updateBook(Long id, Book newBook) {
+        log.info("Updating book ...");
+        Book updatedBook = bookRepository.findById(id)
+                .map(book -> {
+                    book.setBookTitle(newBook.getBookTitle());
+                    book.setBookAuthor(newBook.getBookAuthor());
+                    book.setBookPublicationYear(newBook.getBookPublicationYear());
+                    book.setBookCategory(newBook.getBookCategory());
+                    book.setBookLanguage(newBook.getBookLanguage());
+                    book.setBookPagesNumber(newBook.getBookPagesNumber());
+                    book.setBookTotalCopiesNumber(newBook.getBookTotalCopiesNumber());
+                    book.setBookAvailableCopiesNumber(newBook.getBookAvailableCopiesNumber());
+                    return bookRepository.save(book);
+                })
+                .orElseGet(() -> bookRepository.save(newBook));
+        log.info("Updated book with id: {}", updatedBook.getBookId());
+        return updatedBook;
+    }
+
+    public Book updateBookTotalCopiesNumber(Long id, Integer totalCopiesNumber) {
+        return bookRepository.findById(id)
+                .map(book -> {
+                    book.setBookTotalCopiesNumber(totalCopiesNumber);
+                    return bookRepository.save(book);
+                })
+                .orElseThrow(() -> new EntityNotFoundException("Book not found with id: " + id));
+    }
+
 }
